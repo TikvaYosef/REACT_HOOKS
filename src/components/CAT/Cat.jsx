a9c21f9343b6cea93c3228b9ae417832e3fdf88a
TikvaYosef <tikvayosef13@gmail.com>
2021-12-27 14:35:44 +0200

    commimt

--- src/components/CAT/Cat.jsx
+++ src/components/CAT/Cat.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Cat() {
     let [Cat, setCat] = useState({});
@@ -7,23 +7,29 @@ function Cat() {
         Cat[event.target.name] = event.target.value
     };
 
-    const btn = ()=>{setCat({...Cat});
-};
+    const btn = () => {
+        setCat({ ...Cat });
+    };
 
+    useEffect(
+
+        () => {alert("initalizion") }
+        , [Cat]
+    )
 
     return (
         <div>
             <label htmlFor="text">name</label>
-            <input name="name" type="text"  onChange={showCat}/>
+            <input name="name" type="text" onChange={showCat} />
 
             <label htmlFor="number">age</label>
-            <input name="age" type="number"  onChange={showCat} />
+            <input name="age" type="number" onChange={showCat} />
 
             <label htmlFor="text">color</label>
-            <input name="color" type="text"  onChange={showCat} />
+            <input name="color" type="text" onChange={showCat} />
 
             <label htmlFor="text">gender</label>
-            <input name="gender" type="text"  onChange={showCat} />
+            <input name="gender" type="text" onChange={showCat} />
             <button onClick={btn}>click</button>
 
         </div>
